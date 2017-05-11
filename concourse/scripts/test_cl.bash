b43a5b16933e24ffca7dc1ffd4bfce2a56bb2aa4
Adam Lee <ali@pivotal.io>
2017-05-11 13:40:03 +0800

    install bin

--- concourse/scripts/test_cl.bash
+++ concourse/scripts/test_cl.bash
@@ -1,4 +1,19 @@
 #!/bin/bash -l
-echo "hello"
+function extract_package(){
+	
+SKIP=`awk '/^__END_HEADER__/ {print NR + 1; exit 0; }' $1`
+tail -n +${SKIP} $1 | tar zxf - -C $2
+}
 unzip installer_rhel6_gpdb_clients/greenplum-clients-*.zip
+
+extract_package($(ls installer_rhel6_gpdb_clients/greenplum-clients-*.bin), '~/clients')
+
+cd ~/clients
+source greenplum-path.sh
+
+psql -h SMOKE_TEST_SERVER -C "select version();" postgres
+
+
 unzip installer_rhel6_gpdb_loaders/greenplum-loaders-*.zip
+
+extract_package($(ls installer_rhel6_gpdb_loaders/greenplum-loaders-*.bin), '~/loaders')
