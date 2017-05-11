6d33ba45bd7d16a573d045f20a1a6bfb3cfa8d07
Yuan Zhao <yuzhao@pivotal.io>
2017-05-11 18:05:51 +0800

    execute func
    Signed-off-by: Yuan Zhao <yuzhao@pivotal.io>

--- concourse/scripts/test_cl.bash
+++ concourse/scripts/test_cl.bash
@@ -4,17 +4,16 @@ function extract_package(){
 SKIP=`awk '/^__END_HEADER__/ {print NR + 1; exit 0; }' $1`
 tail -n +${SKIP} $1 | tar zxf - -C $2
 }
-echo `pwd`
 unzip installer_rhel6_gpdb_clients/greenplum-clients-*.zip
+client_bin_file=`ls greenplum-clients-*.bin`
+extract_package "$client_bin_file" "~/clients"
 
-extract_package($(ls installer_rhel6_gpdb_clients/greenplum-clients-*.bin), '~/clients')
-
+pushd
 cd ~/clients
 source greenplum-path.sh
+popd
 
-psql -h SMOKE_TEST_SERVER -C "select version();" postgres
-
-
+psql -h SMOKE_TEST_SERVER -p 5432 -U gpadmin -C "select version();" postgres
 unzip installer_rhel6_gpdb_loaders/greenplum-loaders-*.zip
-
-extract_package($(ls installer_rhel6_gpdb_loaders/greenplum-loaders-*.bin), '~/loaders')
+loader_bin_file=`ls greenplum-loaders-*.bin`
+extract_package "$loader_bin_file" "~/loaders"
