20e1ab2db03eb4985d78f2c6de5211660352becb
Yuan Zhao <yuzhao@pivotal.io>
2017-05-11 17:02:02 +0800

    pwd
    Signed-off-by: Yuan Zhao <yuzhao@pivotal.io>

--- concourse/scripts/test_cl.bash
+++ concourse/scripts/test_cl.bash
@@ -1,9 +1,10 @@
 #!/bin/bash -l
 function extract_package(){
-	
+
 SKIP=`awk '/^__END_HEADER__/ {print NR + 1; exit 0; }' $1`
 tail -n +${SKIP} $1 | tar zxf - -C $2
 }
+echo `pwd`
 unzip installer_rhel6_gpdb_clients/greenplum-clients-*.zip
 
 extract_package($(ls installer_rhel6_gpdb_clients/greenplum-clients-*.bin), '~/clients')
