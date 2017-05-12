a1c69fab30ea49d23a7b1e9563fdf08429feebc5
Yuan Zhao <yuzhao@pivotal.io>
2017-05-12 13:50:15 +0800

    exec func

--- concourse/scripts/test_cl.bash
+++ concourse/scripts/test_cl.bash
@@ -1,21 +1,32 @@
 #!/bin/bash -l
 function extract_package(){
 
-SKIP=`awk '/^__END_HEADER__/ {print NR + 1; exit 0; }' $1`
-tail -n +${SKIP} $1 | tar zxf - -C $2
+  SKIP=`awk '/^__END_HEADER__/ {print NR + 1; exit 0; }' $1`
+  tail -n +${SKIP} $1 | tar zxf - -C $2
 }
 unzip installer_rhel6_gpdb_clients/greenplum-clients-*.zip
 client_bin_file=`ls greenplum-clients-*.bin`
-mkdir -p ~/clients
-extract_package "$client_bin_file" "~/clients"
+client_path="/usr/local/clients"
+mkdir -p $client_path
+extract_package "$client_bin_file" "$client_path"
 
-pushd
-cd ~/clients
-source greenplum-path.sh
-popd
+cd $client_path
+echo `ls`
+source  greenplum_clients_path.sh
+cd -
 
-psql -h SMOKE_TEST_SERVER -p 5432 -U gpadmin -C "select version();" postgres
+psql -h 35.166.116.126 -p 5432 -U gpadmin -c "select version();" postgres
+
 unzip installer_rhel6_gpdb_loaders/greenplum-loaders-*.zip
 loader_bin_file=`ls greenplum-loaders-*.bin`
-mkdir -p ~/loaders
-extract_package "$loader_bin_file" "~/loaders"
+loader_path="/usr/local/loaders"
+mkdir -p $loader_path
+extract_package "$loader_bin_file" "$loader_path"
+
+cd $loader_path
+echo `ls`
+source  greenplum_clients_path.sh
+cd -
+
+psql -h SMOKE_TEST_SERVER -p 5432 -U gpadmin -c "select version();" postgres
+
