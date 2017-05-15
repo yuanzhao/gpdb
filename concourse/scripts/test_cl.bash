04e777ac04ec5c73613aa2020878dd73d6bfb885
Adam Lee <ali@pivotal.io>
2017-05-15 14:43:39 +0800

    trap

--- concourse/scripts/test_cl.bash
+++ concourse/scripts/test_cl.bash
@@ -7,7 +7,7 @@ function extract_package(){
   SKIP=`awk '/^__END_HEADER__/ {print NR + 1; exit 0; }' $1`
   tail -n +${SKIP} $1 | tar zxf - -C $2
   escape_path="${2//\//\\/}"
-  sed_pattern="s/\\\`pwd\\\`/$escape_path/g"
+  sed_pattern='s/`pwd`/$escape_path/g'
   echo $sed_pattern
   sed -i -e $sed_pattern $2/greenplum_clients_path.sh
 }
@@ -22,8 +22,9 @@ source  $client_path/greenplum_clients_path.sh
 echo $STAGING_SERVER_KEY | base64 -d >key
 chmod 400 key
 ssh -M -S /tmp/tunnelsock  -fNT -L 5432:localhost:5432 -i key -o StrictHostKeyChecking=no gpadmin@$STAGING_SERVER_IP
+trap "{ssh -S /tmp/tunnelsock -O exit -i key gpadmin@$STAGING_SERVER_IP; exit 1}" EXIT
 psql -h 35.166.116.126 -p 5432 -U gpadmin -c "select version();" postgres
-ssh -S /tmp/tunnelsock -O exit -i key gpadmin@$STAGING_SERVER_IP
+
 # unzip installer_rhel6_gpdb_loaders/greenplum-loaders-*.zip
 # loader_bin_file=`ls greenplum-loaders-*.bin`
 # loader_path="/usr/local/loaders"
