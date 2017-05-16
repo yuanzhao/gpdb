4bc45a05d99752c982afbff020200d27c53c738e
Adam Lee <ali@pivotal.io>
2017-05-16 10:34:49 +0800

    multi plat

--- concourse/scripts/test_cl.bash
+++ concourse/scripts/test_cl.bash
@@ -16,16 +16,16 @@ function test_package(){
 set +x && echo $STAGING_SERVER_KEY | base64 -d >key && set -x
 chmod 400 key
 ssh -M -S /tmp/tunnelsock  -fNT -L 5432:localhost:5432 -i key -o StrictHostKeyChecking=no gpadmin@$STAGING_SERVER_IP
-trap "{ ssh -S /tmp/tunnelsock -O exit -i key gpadmin@$STAGING_SERVER_IP; exit 1; }" EXIT
+export EXIT_CODE=1
+trap "{ ssh -S /tmp/tunnelsock -O exit -i key gpadmin@$STAGING_SERVER_IP; exit $EXIT_CODE; }" EXIT
 
-unzip installer_rhel6_gpdb_clients/greenplum-clients-*.zip
+unzip installer_gpdb_clients/greenplum-clients-*.zip
 client_bin_file=`ls greenplum-clients-*.bin`
 client_path="/usr/local/clients"
 test_package "$client_bin_file" "$client_path" clients
 
-unzip installer_rhel6_gpdb_loaders/greenplum-loaders-*.zip
+unzip installer_gpdb_loaders/greenplum-loaders-*.zip
 loader_bin_file=`ls greenplum-loaders-*.bin`
 loader_path="/usr/local/loaders"
 test_package "$loader_bin_file" "$loader_path" loaders
-
-exit 0
+export EXIT_CODE=0
