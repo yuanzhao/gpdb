127421d972dbd3880770c6f5ade0927ee95a6b6e
Adam Lee <ali@pivotal.io>
2017-05-16 11:45:16 +0800

    support sles

--- concourse/scripts/test_cl.bash
+++ concourse/scripts/test_cl.bash
@@ -13,11 +13,27 @@ function test_package(){
   source $2/greenplum_$3_path.sh
   psql -h localhost -p 5432 -U gpadmin -c "select version();" postgres
 }
+function prep_env_for_sles(){
+    zypper addrepo --no-gpgcheck http://download.opensuse.org/distribution/11.4/repo/oss/ ossrepo
+    zypper -n install unzip 
+
+}
 set +x && echo $STAGING_SERVER_KEY | base64 -d >key && set -x
 chmod 400 key
 ssh -M -S /tmp/tunnelsock  -fNT -L 5432:localhost:5432 -i key -o StrictHostKeyChecking=no gpadmin@$STAGING_SERVER_IP
-export EXIT_CODE=1
-trap "{ ssh -S /tmp/tunnelsock -O exit -i key gpadmin@$STAGING_SERVER_IP; exit $EXIT_CODE; }" EXIT
+trap "{ ssh -S /tmp/tunnelsock -O exit -i key gpadmin@$STAGING_SERVER_IP; exit 1; }" ERROR
+
+case "$TARGET_OS" in 
+  centos)
+      ;;
+  sles)
+      prep_env_for_sles
+      ;;
+  *)
+      echo "only centos, sles and win32 are supported TARGET_OS'es"
+        false
+        ;;
+esac
 
 unzip installer_gpdb_clients/greenplum-clients-*.zip
 client_bin_file=`ls greenplum-clients-*.bin`
@@ -28,4 +44,6 @@ unzip installer_gpdb_loaders/greenplum-loaders-*.zip
 loader_bin_file=`ls greenplum-loaders-*.bin`
 loader_path="/usr/local/loaders"
 test_package "$loader_bin_file" "$loader_path" loaders
-export EXIT_CODE=0
+
+ssh -S /tmp/tunnelsock -O exit -i key gpadmin@$STAGING_SERVER_IP;
+exit 0
